b7d0a86382bfcfbc225fe9b93ecff3909427e028
sunjianhua <laosun@vidahouse.com>
2017-03-23 10:16:40 +0800

    new project

---
+++ Source/UnrealStudy/UnrealStudy.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UnrealStudy.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealStudy, "UnrealStudy" );
---
+++ Source/UnrealStudy/UnrealStudyGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "UnrealStudyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALSTUDY_API AUnrealStudyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
