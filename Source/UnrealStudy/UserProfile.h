d5eb7e3e0c19bc250e97c280f859ed2f0a2b9e46
sunjianhua <sunjianhua1978@163.com>
2017-03-26 23:48:17 +0800

    .

---
+++ Source/UnrealStudy/UserProfile.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "UObject/NoExportTypes.h"
+#include "UserProfile.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, BlueprintType)
+class UNREALSTUDY_API UUserProfile : public UObject
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Stats)
+	float Armor;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Stats)
+	float HpMax;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Stats)
+	FString Name;
+};
