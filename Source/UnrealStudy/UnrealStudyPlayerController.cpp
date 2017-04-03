78adb198c7d29f4677eb2b150343d9e5bac0fb27
sunjianhua <sunjianhua1978@163.com>
2017-04-03 12:51:10 +0800

    .

---
+++ Source/UnrealStudy/UnrealStudyPlayerController.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UnrealStudy.h"
+#include "UnrealStudyPlayerController.h"
+
+
+
+
---
+++ Source/UnrealStudy/UnrealStudyPlayerController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "UnrealStudyPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALSTUDY_API AUnrealStudyPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
